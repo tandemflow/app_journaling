49ce1e67935e51bf2d779f3739aefad8af1ea3ab
Garrett Nelson <garrett@tandemflow.com>
2025-02-12 09:29:06 -0800

    Initial Commit

---
+++ app_journaling/app_journalingTests/app_journalingTests.swift
@@ -0,0 +1,16 @@
+//
+//  app_journalingTests.swift
+//  app_journalingTests
+//
+//  Created by Garrett Nelson on 2/5/25.
+//
+
+import Testing
+
+struct app_journalingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
